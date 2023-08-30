be4611c4f4c3c7c7334e0d95a085654a459d28cd
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-08-30 12:06:23 +0530

    Update AhmedVicky (2).py

--- chatgpt/module/AhmedVicky (2).py
+++ chatgpt/module/AhmedVicky (2).py
@@ -1,30 +1,35 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@app.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/solotreee":  # Not compulsory
-        return
-    try:
-        try:
-            await bot.get_chat_member("solotreee", msg.from_user.id)
-        except UserNotParticipant:
-            if "https://t.me/solotreee".isalpha():
-                link = "https://t.me/solotreee"
-            else:
-                chat_info = await bot.get_chat("solotreee")
-                link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"🚧 ғɪʀsᴛ ᴊᴏɪɴ ᴛʜᴇ ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ⚠️\n┉───┈┈╌╍╌┄┈───┉┉───┈┈╌\n⌯︙W⃟ᴇʟᴄᴏᴍᴇ : {msg.from_user.mention}\n⌯︙🏝️ ʙᴏᴛ ᴄʜᴀɴɴᴇʟ : S⃟ᴏʟᴏ ᴛʀᴇᴇ\n┉───┈┈╌╍╌┄┈───┉┉───┈┈╌",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @cczza !")
+# Create a Pyrogram Client instance
+app = Client(
+    "my_bot",
+    api_id= 29698700,  # Replace with your API ID
+    api_hash="c08d5af866792c7f96e2de2b35ad5a34"  # Replace with your API hash
+)
+
+# Define the channel username that users need to subscribe to
+channel_username = "solotreee"
+
+# Define the handler for incoming private messages
+@app.on_message(filters.incoming & filters.private)
+async def private_message_handler(client, message):
+    # Check if the user is subscribed to the channel
+    is_subscribed = await client.get_chat_member(channel_username, message.from_user.id)
+    if is_subscribed.status == "member" or is_subscribed.status == "administrator" or is_subscribed.status == "creator":
+        # User is subscribed, proceed with handling the message
+        await client.send_message(message.chat.id, "Thank you for subscribing! You can continue chatting.")
+    else:
+        # User is not subscribed, send a message with a subscription button
+        await client.send_message(
+            message.chat.id,
+            f"Please subscribe to @{channel_username} to access this chat.",
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                    InlineKeyboardButton("Subscribe Now", url=f"https://t.me/{channel_username}")
+                ]]
+            )
+        )
+
+# Start the Pyrogram Client
+app.run()
