c529c5776d6b3658ca11ed8f80d672ba84998312
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-08-30 11:59:57 +0530

    Update AhmedVicky (2).py

--- chatgpt/module/AhmedVicky (2).py
+++ chatgpt/module/AhmedVicky (2).py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from pyrogram import Client
+from pyrogram import Client
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not "https://t.me/solotreee":  # Not compulsory
